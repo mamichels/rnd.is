24730cb4dc7f91e965772cb4d7292525101fc514
mamichels <info@marcelmichels.de>
2022-07-29 23:48:07 +0200

    (ci) Change api port to be configurable by setting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{App, HttpServer, web};
 use random_api::api::{number};
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -7,7 +8,14 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .route("/number", web::get().to(number))
     })
-        .bind(("0.0.0.0", 8080))?
+        .bind(("0.0.0.0", resolve_port()))?
         .run()
         .await
+}
+
+fn resolve_port() -> u16 {
+    match env::var("PORT") {
+        Ok(port) => port.parse::<u16>().unwrap(),
+        Err(_) => panic!("Environment variable PORT not set"),
+    }
 }
